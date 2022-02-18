1369c1ea47797a20614c15bb12e1f03eab72f9ba
Lidia BanosPerez <banospel@flip1.engr.oregonstate.edu>
2022-02-17 19:16:23 -0800

    Updated database file

--- database/cs340_banospel.sql
+++ database/cs340_banospel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 16, 2022 at 09:03 PM
+-- Generation Time: Feb 18, 2022 at 03:12 AM
 -- Server version: 10.6.5-MariaDB-log
 -- PHP Version: 7.4.27
 
@@ -24,10 +24,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cashiers`
+-- Table structure for table `Cashiers`
 --
 
-CREATE TABLE `cashiers` (
+CREATE TABLE `Cashiers` (
   `cashier_id` int(6) NOT NULL,
   `cashier_first` varchar(50) NOT NULL,
   `cashier_last` varchar(50) NOT NULL,
@@ -35,10 +35,10 @@ CREATE TABLE `cashiers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Dumping data for table `cashiers`
+-- Dumping data for table `Cashiers`
 --
 
-INSERT INTO `cashiers` (`cashier_id`, `cashier_first`, `cashier_last`, `hourly_rate`) VALUES
+INSERT INTO `Cashiers` (`cashier_id`, `cashier_first`, `cashier_last`, `hourly_rate`) VALUES
 (750235, 'Monica', 'Geller', 22.5),
 (750236, 'Chandler', 'Bing', 18.5),
 (750237, 'Regina', 'Philange', 20.75);
@@ -46,10 +46,10 @@ INSERT INTO `cashiers` (`cashier_id`, `cashier_first`, `cashier_last`, `hourly_r
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customers`
+-- Table structure for table `Customers`
 --
 
-CREATE TABLE `customers` (
+CREATE TABLE `Customers` (
   `customer_id` int(6) NOT NULL,
   `customer_last` varchar(50) NOT NULL,
   `customer_first` varchar(50) NOT NULL,
@@ -61,10 +61,10 @@ CREATE TABLE `customers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Dumping data for table `customers`
+-- Dumping data for table `Customers`
 --
 
-INSERT INTO `customers` (`customer_id`, `customer_last`, `customer_first`, `street`, `email`, `city`, `state`, `zip`) VALUES
+INSERT INTO `Customers` (`customer_id`, `customer_last`, `customer_first`, `street`, `email`, `city`, `state`, `zip`) VALUES
 (159263, 'Geller', 'Ross', '27 Madison', 'rgeller@yahoo.com', 'New York', 'NY', 10019),
 (159264, 'Green', 'Rachel', '23 Park Ave', 'rachelgrn@gmail.com', 'New York', 'NY', 10016),
 (159265, 'Buffay', 'Phoebe', '806 Jefferson', 'phoebo@sbcglobal.net', 'New York', 'NY', 10019);
@@ -72,10 +72,10 @@ INSERT INTO `customers` (`customer_id`, `customer_last`, `customer_first`, `stre
 -- --------------------------------------------------------
 
 --
--- Table structure for table `invoiceItems`
+-- Table structure for table `InvoiceItems`
 --
 
-CREATE TABLE `invoiceItems` (
+CREATE TABLE `InvoiceItems` (
   `invoiceItem_id` int(6) NOT NULL,
   `invoice_id` int(11) NOT NULL,
   `plant_id` int(11) NOT NULL,
@@ -83,10 +83,10 @@ CREATE TABLE `invoiceItems` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Dumping data for table `invoiceItems`
+-- Dumping data for table `InvoiceItems`
 --
 
-INSERT INTO `invoiceItems` (`invoiceItem_id`, `invoice_id`, `plant_id`, `plant_quantity`) VALUES
+INSERT INTO `InvoiceItems` (`invoiceItem_id`, `invoice_id`, `plant_id`, `plant_quantity`) VALUES
 (110000, 100000, 4563, 5),
 (110001, 100001, 4562, 2),
 (110002, 100001, 4563, 1),
@@ -96,10 +96,10 @@ INSERT INTO `invoiceItems` (`invoiceItem_id`, `invoice_id`, `plant_id`, `plant_q
 -- --------------------------------------------------------
 
 --
--- Table structure for table `invoices`
+-- Table structure for table `Invoices`
 --
 
-CREATE TABLE `invoices` (
+CREATE TABLE `Invoices` (
   `invoice_id` int(6) NOT NULL,
   `customer_id` int(6) NOT NULL,
   `cashier_id` int(3) NOT NULL,
@@ -108,10 +108,10 @@ CREATE TABLE `invoices` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Dumping data for table `invoices`
+-- Dumping data for table `Invoices`
 --
 
-INSERT INTO `invoices` (`invoice_id`, `customer_id`, `cashier_id`, `total_price`, `invoice_date`) VALUES
+INSERT INTO `Invoices` (`invoice_id`, `customer_id`, `cashier_id`, `total_price`, `invoice_date`) VALUES
 (100000, 159263, 750237, 62.5, '2022-01-11'),
 (100001, 159264, 750236, 58.4, '2022-02-03'),
 (100003, 159265, 750235, 100.55, '2022-02-14');
@@ -119,20 +119,20 @@ INSERT INTO `invoices` (`invoice_id`, `customer_id`, `cashier_id`, `total_price`
 -- --------------------------------------------------------
 
 --
--- Table structure for table `plants`
+-- Table structure for table `Plants`
 --
 
-CREATE TABLE `plants` (
+CREATE TABLE `Plants` (
   `plant_id` int(4) NOT NULL,
   `plant_name` varchar(50) NOT NULL,
   `plant_price` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
--- Dumping data for table `plants`
+-- Dumping data for table `Plants`
 --
 
-INSERT INTO `plants` (`plant_id`, `plant_name`, `plant_price`) VALUES
+INSERT INTO `Plants` (`plant_id`, `plant_name`, `plant_price`) VALUES
 (4562, 'Easter Cactus', 22.95),
 (4563, 'Moon Cactus', 12.5),
 (4565, 'Star Cactus', 8.75);
@@ -142,37 +142,37 @@ INSERT INTO `plants` (`plant_id`, `plant_name`, `plant_price`) VALUES
 --
 
 --
--- Indexes for table `cashiers`
+-- Indexes for table `Cashiers`
 --
-ALTER TABLE `cashiers`
+ALTER TABLE `Cashiers`
   ADD PRIMARY KEY (`cashier_id`);
 
 --
--- Indexes for table `customers`
+-- Indexes for table `Customers`
 --
-ALTER TABLE `customers`
+ALTER TABLE `Customers`
   ADD PRIMARY KEY (`customer_id`);
 
 --
--- Indexes for table `invoiceItems`
+-- Indexes for table `InvoiceItems`
 --
-ALTER TABLE `invoiceItems`
+ALTER TABLE `InvoiceItems`
   ADD PRIMARY KEY (`invoiceItem_id`),
   ADD KEY `invoiceItems_invoice_id_fk` (`invoice_id`),
   ADD KEY `invoiceItems_plant_id_fk` (`plant_id`);
 
 --
--- Indexes for table `invoices`
+-- Indexes for table `Invoices`
 --
-ALTER TABLE `invoices`
+ALTER TABLE `Invoices`
   ADD PRIMARY KEY (`invoice_id`),
   ADD KEY `invoices_customer_id_fk` (`customer_id`),
   ADD KEY `invoices_cashier_id_fk` (`cashier_id`);
 
 --
--- Indexes for table `plants`
+-- Indexes for table `Plants`
 --
-ALTER TABLE `plants`
+ALTER TABLE `Plants`
   ADD PRIMARY KEY (`plant_id`);
 
 --
@@ -180,33 +180,33 @@ ALTER TABLE `plants`
 --
 
 --
--- AUTO_INCREMENT for table `cashiers`
+-- AUTO_INCREMENT for table `Cashiers`
 --
-ALTER TABLE `cashiers`
+ALTER TABLE `Cashiers`
   MODIFY `cashier_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=750239;
 
 --
--- AUTO_INCREMENT for table `customers`
+-- AUTO_INCREMENT for table `Customers`
 --
-ALTER TABLE `customers`
+ALTER TABLE `Customers`
   MODIFY `customer_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=159266;
 
 --
--- AUTO_INCREMENT for table `invoiceItems`
+-- AUTO_INCREMENT for table `InvoiceItems`
 --
-ALTER TABLE `invoiceItems`
+ALTER TABLE `InvoiceItems`
   MODIFY `invoiceItem_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110005;
 
 --
--- AUTO_INCREMENT for table `invoices`
+-- AUTO_INCREMENT for table `Invoices`
 --
-ALTER TABLE `invoices`
+ALTER TABLE `Invoices`
   MODIFY `invoice_id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100004;
 
 --
--- AUTO_INCREMENT for table `plants`
+-- AUTO_INCREMENT for table `Plants`
 --
-ALTER TABLE `plants`
+ALTER TABLE `Plants`
   MODIFY `plant_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4566;
 
 --
@@ -214,18 +214,18 @@ ALTER TABLE `plants`
 --
 
 --
--- Constraints for table `invoiceItems`
+-- Constraints for table `InvoiceItems`
 --
-ALTER TABLE `invoiceItems`
-  ADD CONSTRAINT `invoiceItems_invoice_id_fk` FOREIGN KEY (`invoice_id`) REFERENCES `invoices` (`invoice_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `invoiceItems_plant_id_fk` FOREIGN KEY (`plant_id`) REFERENCES `plants` (`plant_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `InvoiceItems`
+  ADD CONSTRAINT `invoiceItems_invoice_id_fk` FOREIGN KEY (`invoice_id`) REFERENCES `Invoices` (`invoice_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `invoiceItems_plant_id_fk` FOREIGN KEY (`plant_id`) REFERENCES `Plants` (`plant_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `invoices`
+-- Constraints for table `Invoices`
 --
-ALTER TABLE `invoices`
-  ADD CONSTRAINT `invoices_cashier_id_fk` FOREIGN KEY (`cashier_id`) REFERENCES `cashiers` (`cashier_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `invoices_customer_id_fk` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Invoices`
+  ADD CONSTRAINT `invoices_cashier_id_fk` FOREIGN KEY (`cashier_id`) REFERENCES `Cashiers` (`cashier_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `invoices_customer_id_fk` FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
