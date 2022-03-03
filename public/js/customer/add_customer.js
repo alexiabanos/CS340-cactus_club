207e54fd8a4952140176d3f4cc3466a384632cd0
Lidia BanosPerez <banospel@flip1.engr.oregonstate.edu>
2022-03-02 19:28:17 -0800

    Add Plants/Customers Ajax

--- public/js/customer/add_customer.js
+++ public/js/customer/add_customer.js
@@ -1,29 +1,44 @@
 // Get the objects we need to modify
-let addPlantForm = document.getElementById('add-plant-form-ajax');
+let addcustomerForm = document.getElementById('add-customer-form-ajax');
 
 // Modify the objects we need
-addPlantForm.addEventListener("submit", function(e) {
+addcustomerForm.addEventListener("submit", function(e) {
 
     // Prevent the form from submitting
     e.preventDefault();
 
     // Get form fields we need to get data from
-    let inputPlantName = document.getElementById("input-plant_name");
-    let inputPlantPrice = document.getElementById("input-plant_price");
+    let inputCustomerFirst = document.getElementById("input-customer_first");
+    let inputCustomerLast = document.getElementById("input-customer_last");
+    let inputStreet = document.getElementById("input-street");
+    let inputStreet = document.getElementById("input-street");
+    let inputStreet = document.getElementById("input-street");
+    let inputStreet = document.getElementById("input-street");
+    let inputStreet = document.getElementById("input-street");
 
     // Get the values from the form fields
-    let plantNameValue = inputPlantName.value;
-    let plantPriceValue = inputPlantPrice.value;
+    let customerFirstValue = inputCustomerFirst.value;
+    let customerLastValue = inputCustomerLast.value;
+    let streetValue = inputStreet.value;
+    let streetValue = inputStreet.value;
+    let streetValue = inputStreet.value;
+    let streetValue = inputStreet.value;
+    let streetValue = inputStreet.value;
 
     // Put our data we want to send in a javascript object
     let data = {
-        plant_name: plantNameValue,
-        plant_price: plantPriceValue,
+        customer_first: customerFirstValue,
+        customer_last: customerLastValue,
+        email: emailValue,
+        street: streetValue,
+        city: cityValue,
+        state: stateValue,
+        zip: zipValue,
     }
 
     // Setup our AJAX request
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/add-plant-ajax", true);
+    xhttp.open("POST", "/add-customer-ajax", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
     // Tell our AJAX request how to resolve
@@ -34,8 +49,9 @@ addPlantForm.addEventListener("submit", function(e) {
             addRowToTable(xhttp.response);
 
             // Clear the input fields for another transaction
-            inputPlantName.value = '';
-            inputPlantPrice.value = '';
+            inputcustomerFirst.value = '';
+            inputcustomerLast.value = '';
+            inputHourlyRate.value = '';
         } else if (xhttp.readyState == 4 && xhttp.status != 200) {
             console.log("There was an error with the input.")
         }
@@ -49,53 +65,56 @@ addPlantForm.addEventListener("submit", function(e) {
 addRowToTable = (data) => {
 
     // Get a reference to the current table on the page and clear it out.
-    let currentTable = document.getElementById("plants-table");
+    let currentTable = document.getElementById("customers-table");
 
     // Get the location where we should insert the new row (end of table)
     let newRowIndex = currentTable.rows.length;
 
-    // Get a reference to the new row from the database query (Price object)
+    // Get a reference to the new row from the database query (last object)
     let parsedData = JSON.parse(data);
     let newRow = parsedData[parsedData.length - 1]
 
     // Create a row and 4 cells
     let row = document.createElement("TR");
     let idCell = document.createElement("TD");
-    let plantNameCell = document.createElement("TD");
-    let plantPriceCell = document.createElement("TD");
+    let firstNameCell = document.createElement("TD");
+    let lastNameCell = document.createElement("TD");
+    let hourlyCell = document.createElement("TD");
 
     let deleteCell = document.createElement("TD");
 
     // Fill the cells with correct data
-    idCell.innerText = newRow.plant_id;
-    plantNameCell.innerText = newRow.plant_name;
-    plantPriceCell.innerText = newRow.plant_price;
+    idCell.innerText = newRow.customer_id;
+    firstNameCell.innerText = newRow.customer_first;
+    lastNameCell.innerText = newRow.customer_last;
+    hourlyCell.innerText = newRow.hourly_rate;
 
     deleteCell = document.createElement("button");
     deleteCell.innerHTML = "Delete";
     deleteCell.onclick = function() {
-        deletePerson(newRow.plant_id);
+        deletePerson(newRow.customer_id);
     };
 
     // Add the cells to the row 
     row.appendChild(idCell);
-    row.appendChild(plantNameCell);
-    row.appendChild(plantPriceCell);
+    row.appendChild(firstNameCell);
+    row.appendChild(lastNameCell);
+    row.appendChild(hourlyCell);
     row.appendChild(deleteCell);
 
     // Add a custom row attribute so the deleteRow function can find a newly added row
-    row.setAttribute('data-value', newRow.plant_id);
+    row.setAttribute('data-value', newRow.customer_id);
 
     // Add the row to the table
     currentTable.appendChild(row);
 
-    // Dropdown menu for updating plants
+    // Dropdown menu for updating customers
 
     // Find drop down menu, create a new option, fill data in the option (full name, id),
     // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh
     let selectMenu = document.getElementById("mySelect");
     let option = document.createElement("option");
-    option.text = newRow.plant_name + ' ' + newRow.plant_price;
+    option.text = newRow.customer_first + ' ' + newRow.customer_last;
     option.value = newRow.id;
     selectMenu.add(option);
 
--- public/js/invoiceItem/addInvoiceItem.js
+++ public/js/invoiceItem/addInvoiceItem.js
@@ -1,29 +1,32 @@
 // Get the objects we need to modify
-let addPlantForm = document.getElementById('add-plant-form-ajax');
+let addCashierForm = document.getElementById('add-cashier-form-ajax');
 
 // Modify the objects we need
-addPlantForm.addEventListener("submit", function(e) {
+addCashierForm.addEventListener("submit", function(e) {
 
     // Prevent the form from submitting
     e.preventDefault();
 
     // Get form fields we need to get data from
-    let inputPlantName = document.getElementById("input-plant_name");
-    let inputPlantPrice = document.getElementById("input-plant_price");
+    let inputCashierFirst = document.getElementById("input-cashier_first");
+    let inputCashierLast = document.getElementById("input-cashier_last");
+    let inputHourlyRate = document.getElementById("input-hourly_rate");
 
     // Get the values from the form fields
-    let plantNameValue = inputPlantName.value;
-    let plantPriceValue = inputPlantPrice.value;
+    let cashierFirstValue = inputCashierFirst.value;
+    let cashierLastValue = inputCashierLast.value;
+    let hourlyRateValue = inputHourlyRate.value;
 
     // Put our data we want to send in a javascript object
     let data = {
-        plant_name: plantNameValue,
-        plant_price: plantPriceValue,
+        cashier_first: cashierFirstValue,
+        cashier_last: cashierLastValue,
+        hourly_rate: hourlyRateValue,
     }
 
     // Setup our AJAX request
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/add-plant-ajax", true);
+    xhttp.open("POST", "/add-cashier-ajax", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
     // Tell our AJAX request how to resolve
@@ -34,8 +37,9 @@ addPlantForm.addEventListener("submit", function(e) {
             addRowToTable(xhttp.response);
 
             // Clear the input fields for another transaction
-            inputPlantName.value = '';
-            inputPlantPrice.value = '';
+            inputCashierFirst.value = '';
+            inputCashierLast.value = '';
+            inputHourlyRate.value = '';
         } else if (xhttp.readyState == 4 && xhttp.status != 200) {
             console.log("There was an error with the input.")
         }
@@ -49,53 +53,56 @@ addPlantForm.addEventListener("submit", function(e) {
 addRowToTable = (data) => {
 
     // Get a reference to the current table on the page and clear it out.
-    let currentTable = document.getElementById("plants-table");
+    let currentTable = document.getElementById("cashiers-table");
 
     // Get the location where we should insert the new row (end of table)
     let newRowIndex = currentTable.rows.length;
 
-    // Get a reference to the new row from the database query (Price object)
+    // Get a reference to the new row from the database query (last object)
     let parsedData = JSON.parse(data);
     let newRow = parsedData[parsedData.length - 1]
 
     // Create a row and 4 cells
     let row = document.createElement("TR");
     let idCell = document.createElement("TD");
-    let plantNameCell = document.createElement("TD");
-    let plantPriceCell = document.createElement("TD");
+    let firstNameCell = document.createElement("TD");
+    let lastNameCell = document.createElement("TD");
+    let hourlyCell = document.createElement("TD");
 
     let deleteCell = document.createElement("TD");
 
     // Fill the cells with correct data
-    idCell.innerText = newRow.plant_id;
-    plantNameCell.innerText = newRow.plant_name;
-    plantPriceCell.innerText = newRow.plant_price;
+    idCell.innerText = newRow.cashier_id;
+    firstNameCell.innerText = newRow.cashier_first;
+    lastNameCell.innerText = newRow.cashier_last;
+    hourlyCell.innerText = newRow.hourly_rate;
 
     deleteCell = document.createElement("button");
     deleteCell.innerHTML = "Delete";
     deleteCell.onclick = function() {
-        deletePerson(newRow.plant_id);
+        deletePerson(newRow.cashier_id);
     };
 
     // Add the cells to the row 
     row.appendChild(idCell);
-    row.appendChild(plantNameCell);
-    row.appendChild(plantPriceCell);
+    row.appendChild(firstNameCell);
+    row.appendChild(lastNameCell);
+    row.appendChild(hourlyCell);
     row.appendChild(deleteCell);
 
     // Add a custom row attribute so the deleteRow function can find a newly added row
-    row.setAttribute('data-value', newRow.plant_id);
+    row.setAttribute('data-value', newRow.cashier_id);
 
     // Add the row to the table
     currentTable.appendChild(row);
 
-    // Dropdown menu for updating plants
+    // Dropdown menu for updating cashiers
 
     // Find drop down menu, create a new option, fill data in the option (full name, id),
     // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh
     let selectMenu = document.getElementById("mySelect");
     let option = document.createElement("option");
-    option.text = newRow.plant_name + ' ' + newRow.plant_price;
+    option.text = newRow.cashier_first + ' ' + newRow.cashier_last;
     option.value = newRow.id;
     selectMenu.add(option);
 
